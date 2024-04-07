637794276adb864c808589aee9d965eb2dc66dc1
xjohnson <xjohnsondev@gmail.com>
2024-04-07 10:56:32 -0700

    render update

--- backend/primal.sql
+++ backend/primal.sql
@@ -1,17 +1,17 @@
-\echo 'Delete and recreate primal db?'
+-- \echo 'Delete and recreate primal db?'
 
-DROP DATABASE primal;
-CREATE DATABASE primal ENCODING 'UTF8';
-\connect primal
+-- DROP DATABASE primal;
+-- CREATE DATABASE primal ENCODING 'UTF8';
+-- \connect primal
 
 \i primal-schema.sql
 \i primal-seed.sql
 
-primal
-\echo 'Delete and recreate primal_test db?'
+-- primal
+-- \echo 'Delete and recreate primal_test db?'
 
-DROP DATABASE primal_test;
-CREATE DATABASE primal_test ENCODING 'UTF8';
-\connect primal_test
+-- DROP DATABASE primal_test;
+-- CREATE DATABASE primal_test ENCODING 'UTF8';
+-- \connect primal_test
 
-\i primal-schema.sql
+-- \i primal-schema.sql
