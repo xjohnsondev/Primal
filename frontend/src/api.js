6655acf2d3ccb890d8ad327f8601de06ca4604a6
xjohnson <xjohnsondev@gmail.com>
2024-04-07 01:45:00 -0700

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://primal-server.onrender.com:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://primal-server.onrender.com";
 
 /** API Class.
  *
