15e46d6e0253787a135cdb4b452b8af4f35a1534
xjohnson <xjohnsondev@gmail.com>
2024-04-07 02:06:09 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
-const { exec } = require("child_process");
 
-// Define the path to the primal.sql file
-const sqlFilePath = "./primal.sql";
-
-// Execute the SQL script as a child process
-exec(`psql -f ${sqlFilePath}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing SQL script: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Error executing SQL script: ${stderr}`);
-    return;
-  }
-  console.log("SQL script executed successfully");
-  
-  // Start the server after the SQL script has been executed
-  app.listen(PORT, function () {
-    console.log(`Started on https://primal-server.onrender.com:${PORT}`);
-  });
+app.listen(PORT, function () {
+  console.log(`Started on https://primal-server.onrender.com:${PORT}`);
 });
